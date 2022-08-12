404e926aef57e47fb4940f95168079c79082312a
LENOVO <LENOVO@LAPTOP-7M7CPIOL>
2022-08-12 12:12:05 +0700

    first commit

---
+++ src/main/java/Circle.java
@@ -0,0 +1,15 @@
+public class Circle implements GeomtricObject {
+    protected double radius = 1.0;
+    
+ public Circle(double radius){
+     this.radius = radius;
+ }
+    @Override
+ public double getPerimeter(){
+ return 2*Math.PI*radius;
+ }
+    @Override
+ public double getArea(){
+ return Math.PI*Math.pow(radius, 2);
+ }
+}
