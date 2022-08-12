0aa70db159c68ef355224a60088f4049eef1ba3c
LENOVO <LENOVO@LAPTOP-7M7CPIOL>
2022-08-12 12:15:14 +0700

    first commit

---
+++ src/main/java/ResizableCircle.java
@@ -0,0 +1,11 @@
+
+public class ResizableCircle extends Circle implements Resizable {
+    public ResizableCircle (double radius){
+        super(radius);
+    }
+
+    @Override
+    public void resize(int percent) {
+       this.radius = percent * 0.01*super.radius;
+    }
+}
